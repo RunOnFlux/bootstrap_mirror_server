b0ab79e9f1cd9198174f0df2043fd46cb13a93f1
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2022-03-21 16:19:48 +0100

    cleanup

--- screen_check.sh
+++ screen_check.sh
@@ -3,11 +3,8 @@
 source $(dirname $(readlink -f $0))/bootstrap_config
 
 if [[ "$screen_enable" == "1" ]]; then
-
   screen_check=$(screen -ls | grep http_server | wc -l)
-  
   if [[ screen_check == "0" ]]; then
-
    source $(dirname $(readlink -f $0))/bootstrap_config
    bash /$home_dir/bootstrap_mirror_server/discord.sh \
   --webhook-url="$web_hook_url" \
@@ -17,6 +14,5 @@ if [[ "$screen_enable" == "1" ]]; then
   --field "Server;$server_name" \
   --field "Info;Screen problem detected!" \
   --text "Ping: $user_id_list"
-  
   fi
 fi
